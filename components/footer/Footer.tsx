2897db03ae90d6a04d835962ebc4e903236c2f86
jhonswg <irfanmahmudi98@gmail.com>
2025-10-17 01:34:11 +0700

    footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,48 +1,119 @@
 import { FC } from "react";
 import {
-  BoxProps,
-  HStack,
-  Tag,
-  Text,
-  useColorModeValue,
+  Box,
+  Flex,
   VStack,
+  HStack,
+  Text,
+  Link as ChakraLink,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { Logo } from "@/components/logo";
-import { Link } from "@/components/link";
-import { NicaraguaFlag } from "@/components/nicaragua-flag";
 import { siteConfig } from "@/config";
 
-const Footer: FC<BoxProps> = (props) => {
-  const textColor = useColorModeValue("gray.500", "gray.300");
+const Footer: FC = (props) => {
+  const borderColor = useColorModeValue("gray.200", "gray.700");
+  const textColor = useColorModeValue("gray.600", "gray.300");
+  const linkHover = useColorModeValue("brand.600", "brand.300");
+  
 
   return (
-    <VStack
-      as="footer"
-      alignItems="start"
-      spacing="5"
-      borderTop="1px"
-      borderColor={useColorModeValue("gray.200", "gray.700")}
-      mt="12"
-      py="8"
-      {...props}
-    >
-      <Logo />
-      {siteConfig.author && (
-        <HStack fontSize="sm" fontWeight="medium" color={textColor} mt="4">
-          <span>Crafted in</span>
-          <NicaraguaFlag />
-          <span>
-            by{" "}
-            <Link
-              isExternal
-              href={`https://www.github.com/${siteConfig.author?.github}`}
+    <Box as="footer" mt="16" {...props}>
+      <Flex
+        px={{ base: 6, md: 12 }}
+        py={{ base: 10, md: 16 }}
+        direction={{ base: "column", md: "row" }}
+        justify="space-between"
+        align={{ base: "start", md: "start" }}
+        gap={10}
+        mb="-5"
+      >
+        {/* LEFT: Logo + Description */}
+        <VStack align="start" spacing={4} maxW="lg">
+          <Logo />
+          <Text color={textColor}>
+            Jhonswg offers reliable blockchain infrastructure with a focus on
+            security, performance, and validator node services.
+          </Text>
+          <ChakraLink href="mailto:me@jhonswg.com" _hover={{ color: linkHover }}>
+              me@jhonswg.com
+            </ChakraLink>
+        </VStack>
+
+        {/* RIGHT: Link sections */}
+        <Flex wrap="wrap" gap={8} justify="flex-end">
+          <VStack align="start">
+            <Text fontWeight="bold">Products</Text>
+            <ChakraLink href="/staking" _hover={{ color: linkHover }}>
+              Staking
+            </ChakraLink>
+            <ChakraLink href="/nodes" _hover={{ color: linkHover }}>
+              Nodes
+            </ChakraLink>
+            <ChakraLink href="/validators" _hover={{ color: linkHover }}>
+              Validators
+            </ChakraLink>
+          </VStack>
+          <VStack align="start">
+            <Text fontWeight="bold">Company</Text>
+            <ChakraLink href="/about" _hover={{ color: linkHover }}>
+              About Us
+            </ChakraLink>
+            <ChakraLink href="/team" _hover={{ color: linkHover }}>
+              Team
+            </ChakraLink>
+            <ChakraLink href="/careers" _hover={{ color: linkHover }}>
+              Careers
+            </ChakraLink>
+          </VStack>
+          <VStack align="start">
+            <Text fontWeight="bold">Resources</Text>
+            <ChakraLink href="/blog" _hover={{ color: linkHover }}>
+              Blog
+            </ChakraLink>
+            <ChakraLink href="/docs" _hover={{ color: linkHover }}>
+              Documentation
+            </ChakraLink>
+            <ChakraLink href="/contact" _hover={{ color: linkHover }}>
+              Contact
+            </ChakraLink>
+          </VStack>
+        </Flex>
+      </Flex>
+
+      {/* Footer bottom bar */}
+      <Box borderTop="1px" borderColor={borderColor} py="6">
+        <Flex
+          px={{ base: 6, md: 12 }}
+          align="center"
+          justify="space-between"
+          direction={{ base: "column", md: "row" }}
+          gap={4}
+        >
+          <Text fontSize="sm" color={textColor}>
+            &copy; {new Date().getFullYear()} Jhonswg. All rights reserved.
+          </Text>
+          <HStack spacing={4}>
+            <ChakraLink
+              href="/privacy"
+              fontSize="sm"
+              color={textColor}
+              _hover={{ color: linkHover }}
             >
-              {siteConfig.author?.name}
-            </Link>
-          </span>
-        </HStack>
-      )}
-    </VStack>
+              Privacy Policy
+            </ChakraLink>
+            <ChakraLink
+              href="/terms"
+              fontSize="sm"
+              color={textColor}
+              _hover={{ color: linkHover }}
+            >
+              Terms of Service
+            </ChakraLink>
+          </HStack>
+        </Flex>
+      </Box>
+    </Box>
   );
 };
 
