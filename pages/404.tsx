2dc1aab2247227c0935b403b7fda4cf3af8202d6
jhonswg <irfanmahmudi98@gmail.com>
2025-10-23 14:03:51 +0700

    404

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "20vh" }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Next.js tidak membuild halaman ini di Vercel.</p>
+      </div>
+    );
+  }
+  
