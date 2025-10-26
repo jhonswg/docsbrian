5fac1b5720fb2ad7533519dec64a768cc02603f7
jhonswg <irfanmahmudi98@gmail.com>
2025-10-26 12:28:46 +0700

    navbarrender

--- components/sidebar/empeiria/EmpeiriaLayout.tsx
+++ components/sidebar/empeiria/EmpeiriaLayout.tsx
@@ -1,19 +1,29 @@
 import {
   Box,
   Flex,
+  HStack,
+  Text,
+  Button,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { Navbar } from "@/components/navbar";
+import { navbarRoutes } from "@/config/navbar-routes";
 import { Footer } from "@/components/footer";
 import { EmpeiriaSidebar } from "@/components/sidebar/empeiria/EmpeiriaSidebar";
 import { ReactNode } from "react";
+import NextLink from "next/link";
+import { Link as ChakraLink } from "@chakra-ui/react";
 import EmpeiriaNetworkBox from "@/components/sidebar/empeiria/EmpeiriaNetworkBox";
 
 export default function EmpeiriaLayout({ children }: { children: ReactNode }) {
   const border = useColorModeValue("whiteAlpha.300", "whiteAlpha.200");
   const sidebarBg = useColorModeValue("white", "gray.800");
+  const cardBg = useColorModeValue("whiteAlpha.50", "whiteAlpha.100");
 
   return (
     <>
+      <Navbar routes={navbarRoutes} />
+
       <Box
         position="relative"
         minH="100vh"
@@ -21,6 +31,26 @@ export default function EmpeiriaLayout({ children }: { children: ReactNode }) {
         pb="16"
         overflow="visible"
       >
+        {/* Background gradient blur */}
+        {/* <Box
+          boxSize="72"
+          position="absolute"
+          top="0"
+          left="0"
+          bg="purple.500"
+          borderRadius="full"
+          filter="blur(400px)"
+        />
+        <Box
+          boxSize="72"
+          position="absolute"
+          bottom="0"
+          right="0"
+          bg="pink.500"
+          borderRadius="full"
+          filter="blur(400px)"
+        /> */}
+
         {/* Main content */}
         <Flex
           position="relative"
